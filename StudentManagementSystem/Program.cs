755b038780315555e6131d75f28243ece500b13d
Precious Nwajei <peculiarpreciouslee@gmail.com>
2026-03-11 21:41:50 +0100

    Task 1 & 2: Implemented list-based storage and menu with Add and View functionality

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -1,22 +1,126 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Student Management System
 
-var studentlist = new[]
-{
-    new { ID= 1, Name = "Precious", Score = (double?)95.3, IsEnrolled = (bool?)true },
-    new { ID= 2, Name = "Joy",   Score = (double?)79.8, IsEnrolled = (bool?)true },
-    new { ID= 3,Name = "Sandra",  Score = (double?)59.5, IsEnrolled = (bool?)false },
-    new { ID= 4,Name = "Sone", Score = (double?)null, IsEnrolled = (bool?)null },
-    new { ID= 5,Name = "Blessing", Score = (double?)null, IsEnrolled = (bool?)null }
-} ;
+List<int> studentIDs = new List<int> { 1, 2, 3, 4, 5 };
+List<string> studentNames = new List<string> { "David Ayeni", "Joy Osaghae", "Sandra Ike", "Sone Adibeli", "Blessing Okafor" };
+List<double> studentScores = new double[] { 95.3, 60.8, 59.5, 45.0, 30.0 }.ToList();
+List<char> Status = new List<Char> { 'A', 'B', 'C', 'D', 'F' };
 
-int numberOfStudent = studentlist.Length;
-Console.WriteLine($"Number of Student: {numberOfStudent}");
+// 2. Number of students (int)
+int numberOfStudents = studentIDs.Count;
 
-foreach (var student in studentlist)
+
+bool IsActiveMenu = true;
+
+while (IsActiveMenu)
 {
-    string scorevalue = student.Score.HasValue ? student.Score.Value.ToString(): "N/A";
-    string studentStatus = student.IsEnrolled.HasValue 
-    ? (student.IsEnrolled.Value ? "Enrolled" : "Not Enrolled") : "Pending";    // If IsEnrolled is null, it will display "Pending"
-    Console.WriteLine($"Student ID: {student.ID}, Student Name: {student.Name}, Score: {scorevalue}, Status:  {studentStatus}  ");
+    Console.WriteLine("\nChoose a Menu:");
+    Console.WriteLine("1. Add new student");
+    Console.WriteLine("2. View all students");
+    Console.WriteLine("3. Calculate average grade");
+    Console.WriteLine("4. Find student by ID");
+    Console.WriteLine("5. Update student grade");
+    Console.WriteLine("6. Delete student");
+    Console.WriteLine("7. Display statistics");
+    Console.WriteLine("8. Exit");
+    Console.Write("Enter your choice: ");
+
+    string? input = Console.ReadLine();
+    bool isValidInt = int.TryParse(input, out int userInput);
+
+    switch (userInput)
+    {   // Add New Student
+        case 1:
+            Console.WriteLine("\n--- Add New Student ---");
+
+            int nextID = studentIDs.Count + 1; // Automatic StudentID to follow the order, instead of prompting the user to enter it
+            Console.Write("Enter Student Name: ");
+
+            string? studentName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(studentName))
+            {
+                Console.WriteLine("Error: Name cannot be empty.");
+                break;
+            }
+            // 3. Validate Score (Must be double between 0-100)
+            Console.Write("Enter Student Score (0-100): ");
+            if (!double.TryParse(Console.ReadLine(), out double studentScore) || studentScore < 0 || studentScore > 100)
+            {
+                Console.WriteLine("Error: Score must be a number between 0 and 100.");
+                break;
+            }
+
+            char statusInput ;
+            if (studentScore >= 70)
+            {
+                statusInput = 'A';
+            }
+            else if (studentScore >= 60) // If it's 60 to 69
+            {
+                statusInput = 'B';
+            }
+            else if (studentScore >= 50) // If it's 50 to 59
+            {
+                statusInput = 'C';
+            }
+            else if (studentScore >= 45) // If it's 45 to 49
+            {
+                statusInput = 'D';
+            }
+            else if (studentScore >= 40) // If it's 40 to 44
+            {
+                statusInput = 'E';
+            }
+            else // Anything below 40
+            {
+                statusInput = 'F';
+            }
+
+            //ADD to the lists
+            studentIDs.Add(nextID);
+            studentNames.Add(studentName);
+            studentScores.Add(studentScore);
+            Status.Add(statusInput);
+
+            // 4. Confirm Success
+            Console.WriteLine($"\nSUCCESS: Student '{studentName}' (Student ID: {nextID}) added with score {studentScore}{statusInput}.");
+            break;
+
+        //View All Students
+        case 2:
+             Console.WriteLine("\n--- Student List ---");
+
+             for (int i = 0; i < studentIDs.Count; i++)
+             {
+                 Console.WriteLine($"ID: {studentIDs[i]}, Name: {studentNames[i]}, Score: {studentScores[i]}, Status: {Status[i]}");
+             }
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        case 8:
+            IsActiveMenu = false;
+            Console.WriteLine("Exiting program...");
+            break;
+
+        default:
+            Console.WriteLine("Invalid selection, please try again.");
+            break;
+    }
+
+
+
 }
